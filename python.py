16acfe051bbc08ad0864a58ac5752c94fbb016b9
menahakim <97699739+menahakim@users.noreply.github.com>
2024-04-16 14:54:38 -0400

    Update python.py

--- python.py
+++ python.py
@@ -175,8 +175,9 @@ def find_player_hometown(driver):
     # Adjusted query to retrieve the player's home_town_id property
     hometown_query = f"""
     MATCH (p:Player)
-    WHERE p.team_roster_name = p.name
-    RETURN p.home_town_id
+    WHERE p.team_roster_name = 'Specific Team Name'
+    RETURN p.name, p.home_town_id
+
     """
     hometown_result = run_neo4j_query(driver, hometown_query)
 
