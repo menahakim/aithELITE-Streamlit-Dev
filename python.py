149c338a3bd5c207ef19ff954b10cf949b2434b4
menahakim <97699739+menahakim@users.noreply.github.com>
2024-04-16 14:32:19 -0400

    Update python.py

--- python.py
+++ python.py
@@ -6,9 +6,9 @@ from neo4j import GraphDatabase
 import pandas as pd
 
 # Neo4j connection parameters
-uri = ""
-user = ""
-password = ""
+uri = "neo4j+s://722a0a63.databases.neo4j.io:7687"
+user = "neo4j"
+password = "1z9uUXRk4_WQxCbSpJE3qFiJPqRZPeyTXjyFa5kqeZA"
 
 
 logo_path = "images/aitheletego.png"
